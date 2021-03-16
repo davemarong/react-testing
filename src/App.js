ced63c52766dd58cf647d42df840cc66fdb4c87f
davemarong <kmarong@hotmail.com>
2021-03-16 21:41:40 +0100

    CREATE -- Counter app and name catcher app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import Counter from "./Counter";
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [myName, setMyname] = useState("Missingo");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Counter
+        counter={counter}
+        setCounter={setCounter}
+        myName={myName}
+        setMyname={setMyname}
+      />
+    </>
   );
 }
 

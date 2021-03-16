2fd81f3b24fdd210cdb79ff81317e11e28620fbe
Dave Marong <kmarong@hotmail.com>
2021-03-16 21:43:28 +0100

    New start

--- src/App.js
+++ src/App.js
@@ -1,18 +1,5 @@
 import React, { useState } from "react";
-import Counter from "./Counter";
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [myName, setMyname] = useState("Missingo");
-  return (
-    <>
-      <Counter
-        counter={counter}
-        setCounter={setCounter}
-        myName={myName}
-        setMyname={setMyname}
-      />
-    </>
-  );
+  return null;
 }
-
 export default App;
